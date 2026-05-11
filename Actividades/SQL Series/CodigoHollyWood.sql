95b42ca93e97004a9709f91f8c3b47b2e7881058
Alexis <alexis.billietet32@gmail.com>
2026-05-11 09:58:14 -0300

    Add files via upload

---
+++ Actividades/SQL Series/CodigoHollyWood.sql
@@ -0,0 +1,49 @@
+create database SalaKebrado;
+-- drop database SalaKebrado
+use SalaKebrado;
+
+create table directores
+(
+ IDDirector int primary key auto_increment not null,
+ nombre varchar(25),
+ Edad int(2),
+ Sueldo float
+);
+create table actores
+(
+ IDActor int primary key auto_increment not null,
+ nombre varchar(25),
+ Apellido varchar(25)
+);
+create table series
+( 
+ IDSerie int primary key auto_increment not null,
+ nombre varchar(25),
+ GeneroPrincipal varchar(25),
+ AñoLanzamiento year,
+ Presupuesto float,
+ IDDirector int,
+ foreign key (IDDirector) references directores(IDDirector)
+);
+create table plataformas
+(
+ IDPlataforma int primary key auto_increment not null,
+ nombre varchar(25),
+ AñoLanzamiento year
+);
+create table series_plataformas
+(
+ IDSerie_Plataforma int primary key auto_increment not null,
+ IDPlataforma int,
+ foreign key (IDPlataforma) references plataformas(IDPlataforma),
+ IDSerie int,
+ foreign key (IDSerie) references series(IDSerie)
+);
+create table personajes
+(
+ IDSerie_Plataforma int primary key auto_increment not null,
+ IDPlataforma int,
+ foreign key (IDPlataforma) references plataformas(IDPlataforma),
+ IDSerie int,
+ foreign key (IDSerie) references series(IDSerie)
+)
